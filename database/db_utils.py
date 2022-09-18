837e216f03ed60a6a5d4000e9dec207305caaa54
Evgeny Petrov <odis743@gmail.com>
2022-09-18 13:28:45 +0300

    TelegramBot diploma

--- database/db_utils.py
+++ database/db_utils.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 import json
+from json import JSONDecodeError
 
 
 def add_history_row(cmd: str, cmd_time: datetime, hotels: list[str]):
     """Добавляет в базу запись о команде"""
-    
-    with open("database/history.json", "rb") as f:
-        data = json.load(f)
+
+    try:
+        with open("database/history.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        f = open("database/history.json", "w")
+        f.close()
+        data = {"history": []}
+    except JSONDecodeError:
+        data = {"history": []}
     
     data["history"].append({
         "cmd": cmd,
